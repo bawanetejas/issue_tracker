f42330f05b815c977a71d4162acd5fccdca8f872
bawanetejas <tejasbawane221@gmail.com>
2026-05-24 16:19:41 +0530

    refactor: replace anchor tags with react router links

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,77 +1,83 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Sidebar() {
+    const currentRoute = useLocation();
+
+    function checkRoute(route) {
+        return currentRoute.pathname === route
+    }
+
     return (
         <div
-            class="hidden md:flex flex-col h-screen w-64 bg-surface-container-low border-r border-outline-variant p-md gap-sm shrink-0"
+            className="hidden md:flex flex-col h-screen w-64 bg-surface-container-low border-r border-outline-variant p-md gap-sm shrink-0"
         >
-            <div class="flex items-center gap-md mb-xl">
+            <div className="flex items-center gap-md mb-xl">
                 <div
-                    class="w-10 h-10 bg-primary-container rounded-lg flex items-center justify-center"
+                    className="w-10 h-10 bg-primary-container rounded-lg flex items-center justify-center"
                 >
                     <span
-                        class="material-symbols-outlined text-on-primary-container"
+                        className="material-symbols-outlined text-on-primary-container"
                         data-icon="track_changes"
                     >track_changes</span>
                 </div>
                 <div>
-                    <h1 class="font-headline-sm text-headline-sm font-bold text-primary">
+                    <h1 className="font-headline-sm text-headline-sm font-bold text-primary">
                         TrackFlow
                     </h1>
-                    <p class="font-label-md text-label-md text-on-surface-variant">
+                    <p className="font-label-md text-label-md text-on-surface-variant">
                         Internal Utility
                     </p>
                 </div>
             </div>
-            <nav class="flex flex-col gap-xs flex-1">
-                <a
-                    class="flex items-center gap-md p-md bg-secondary-container text-on-secondary-container rounded-lg active:scale-95 duration-150 transition-all"
-                    href="#"
+            <nav className="flex flex-col gap-xs flex-1">
+                <Link to="/dashboard"
+                    className={`flex items-center gap-md p-md ${checkRoute("/dashboard") ? "bg-secondary-container" : "hover:bg-surface-container-highest"} text-on-secondary-container rounded-lg active:scale-95 duration-150 transition-all`}
                 >
-                    <span class="material-symbols-outlined" data-icon="dashboard"
+                    <span className="material-symbols-outlined" data-icon="dashboard"
                     >dashboard</span>
-                    <span class="font-label-md text-label-md">Dashboard</span>
-                </a>
-                <a
-                    class="flex items-center gap-md p-md text-on-surface-variant hover:text-on-surface hover:bg-surface-container-highest rounded-lg active:scale-95 duration-150 transition-all"
-                    href="#"
+                    <span className="font-label-md text-label-md">Dashboard</span>
+                </Link>
+                <Link to="/teams"
+                    className={`flex items-center gap-md p-md ${checkRoute("/teams") ? "bg-secondary-container" : "hover:bg-surface-container-highest"} text-on-secondary-container rounded-lg active:scale-95 duration-150 transition-all`}
+
                 >
-                    <span class="material-symbols-outlined" data-icon="group">group</span>
-                    <span class="font-label-md text-label-md">Teams</span>
-                </a>
-                <a
-                    class="flex items-center gap-md p-md text-on-surface-variant hover:text-on-surface hover:bg-surface-container-highest rounded-lg active:scale-95 duration-150 transition-all"
-                    href="#"
+                    <span className="material-symbols-outlined" data-icon="group">group</span>
+                    <span className="font-label-md text-label-md">Teams</span>
+                </Link>
+                <Link to="/issues"
+                    className={`flex items-center gap-md p-md ${checkRoute("/issues") ? "bg-secondary-container" : "hover:bg-surface-container-highest"} text-on-secondary-container rounded-lg active:scale-95 duration-150 transition-all`}
+
                 >
-                    <span class="material-symbols-outlined" data-icon="bug_report"
+                    <span className="material-symbols-outlined" data-icon="bug_report"
                     >bug_report</span>
-                    <span class="font-label-md text-label-md">Issues</span>
-                </a>
+                    <span className="font-label-md text-label-md">Issues</span>
+                </Link>
             </nav>
             <div
-                class="mt-auto flex flex-col gap-xs pt-md border-t border-outline-variant"
+                className="mt-auto flex flex-col gap-xs pt-md border-t border-outline-variant"
             >
                 <button
-                    class="flex items-center gap-md w-full bg-primary text-on-primary font-bold py-md px-lg rounded-lg hover:bg-primary/90 transition-colors mb-md active:scale-95 duration-150"
+                    className="flex items-center gap-md w-full bg-primary text-on-primary font-bold py-md px-lg rounded-lg hover:bg-primary/90 transition-colors mb-md active:scale-95 duration-150"
                 >
-                    <span class="material-symbols-outlined" data-icon="add">add</span>
-                    <span class="font-label-md text-label-md">Create Issue</span>
+                    <span className="material-symbols-outlined" data-icon="add">add</span>
+                    <span className="font-label-md text-label-md">Create Issue</span>
                 </button>
                 <a
-                    class="flex items-center gap-md p-md text-on-surface-variant hover:text-on-surface hover:bg-surface-container-highest rounded-lg active:scale-95 duration-150 transition-all"
+                    className="flex items-center gap-md p-md text-on-surface-variant hover:text-on-surface hover:bg-surface-container-highest rounded-lg active:scale-95 duration-150 transition-all"
                     href="#"
                 >
-                    <span class="material-symbols-outlined" data-icon="settings"
+                    <span className="material-symbols-outlined" data-icon="settings"
                     >settings</span>
-                    <span class="font-label-md text-label-md">Settings</span>
+                    <span className="font-label-md text-label-md">Settings</span>
                 </a>
                 <a
-                    class="flex items-center gap-md p-md text-on-surface-variant hover:text-on-surface hover:bg-surface-container-highest rounded-lg active:scale-95 duration-150 transition-all"
+                    className="flex items-center gap-md p-md text-on-surface-variant hover:text-on-surface hover:bg-surface-container-highest rounded-lg active:scale-95 duration-150 transition-all"
                     href="#"
                 >
-                    <span class="material-symbols-outlined" data-icon="logout"
+                    <span className="material-symbols-outlined" data-icon="logout"
                     >logout</span>
-                    <span class="font-label-md text-label-md">Logout</span>
+                    <span className="font-label-md text-label-md">Logout</span>
                 </a>
             </div>
         </div>
